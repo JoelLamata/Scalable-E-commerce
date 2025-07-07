5313b4e47b2e09f8ea7ecb5edc87a90c022d2751
joel <jlamata88@gmail.com>
2025-07-07 16:08:41 +0200

    init project

---
+++ src/main/java/scalable/ecommerce/secProject/SecProjectApplication.java
@@ -0,0 +1,13 @@
+package scalable.ecommerce.secProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecProjectApplication.class, args);
+	}
+
+}
